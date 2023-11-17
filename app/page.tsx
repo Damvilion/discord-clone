7b73b9cd6016182b971e72f448ea094731a0dec7
Noel Ohaeri <noelohaeri@gmail.com>
2023-11-17 14:07:29 -0500

    Starter Files

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+import { link } from 'fs';
+
+export default function Home() {
+    return (
+        <main className='text-3xl text-indigo-500 font-bold'>
+            <p>Hello Discord Clone</p>
+            <Button variant='link'> HELLO</Button>
+        </main>
+    );
+}
